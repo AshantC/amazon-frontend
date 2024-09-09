6eae80bd75f94fc6cf91c94d6baa9a56ca5cba9a
AshantC <ashantchaudhary22@gmail.com>
2024-09-09 22:06:26 +0545

    NavbarBanner added

--- src/components/navbar/navbarBanner/NavbarBanner.jsx
+++ src/components/navbar/navbarBanner/NavbarBanner.jsx
@@ -1,15 +1,48 @@
 import React from 'react';
 import "./NavbarBanner.css";
 import MenuIcon from '@mui/icons-material/Menu';
+import amazonPrime from "../../../assets/amazonPrimeLogo.jpg"
 
 const NavbarBanner = () => {
+  const options = [
+    { "name": "Fresh" },
+    { "name": "Amazon Mini" },
+    { "name": "Sell" },
+    { "name": "Best Sellers" },
+    { "name": "Today's Deal" },
+    { "name": "Mobiles" },
+    { "name": "Electronics" },
+    { "name": "Prime" },
+    { "name": "Customer Service" },
+    { "name": "Prime" },
+    { "name": "Electronics" },
+    { "name": "Fashion" },
+    // { "name": "New Releases" },
+  ]
   return (
     <div className='navbarBanner'>
       <div className="navbarBannerOptionLeft">
-        <div className='optionNav'></div>
+
+        <div className='optionNavbarBanner'>
+          <MenuIcon sx={{ fontSize: "24px" }} />
+          <div className="allOptionNavbarBanner">All</div>
+        </div>
+
+        {
+          options.map((item, index) => {
+            return (
+              <div className='optionNavbarBanner' key={index}>
+                <div className="allOptionNavbarBanner">{item.name}</div>
+              </div>
+            )
+          })
+        }
+
+
       </div>
+
       <div className="navbarBannerRightSide">
-        <img src="https://f.media-amazon.com/images/G/img19/SiddMiniTV/26MayHero/SWM_400x391._CB557661637_.jpg" alt="amazonPrime Logo" />
+        <img src={amazonPrime} alt="amazonPrime Logo" />
       </div>
     </div>
   )
